3a28718ae6b780c74c639a3830f3e78f188c7ae5
jeff-romero <jeffromero86@gmail.com>
2026-06-22 14:36:16 -0700

    migrate navigation progress from old repo

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './navigation.html',
   styleUrl: './navigation.css',
 })
-export class Navigation {}
+export class Navigation implements OnInit {
+  bankName = "Balanced Banking";
+  checkingSavings = "Checking and Savings";
+  services = "Services";
+  LOG_IN = "Log In";
+  MY_ACCOUNT = "My Account";
+  logInStatus = this.LOG_IN;
+  // property binding - class
+  LOCK_ICON = "fa fa-lock";
+  ACCOUNT_MANAGEMENT_ICON = "fa fa-cog";
+  loginStatus = "fa fa-lock";
+  logInStatusIcon = this.LOCK_ICON;
+  @Input() isLoggedIn:boolean = false;
+
+  constructor() {
+    // let checkingSavingsDropdown = document.getElementById('checking-savings-dropdown') as HTMLSpanElement;
+    // checkingSavingsDropdown.addEventListener("mouseover", (e) => {
+
+    // });
+  }
+
+  ngOnInit() {
+    if (this.isLoggedIn) {
+      this.logInStatus = this.MY_ACCOUNT;
+      this.logInStatusIcon = this.ACCOUNT_MANAGEMENT_ICON;
+    }
+  }
+
+  hover(event: PointerEvent): void {
+    event.preventDefault();
+    console.log("hovered");
+  }
+}
