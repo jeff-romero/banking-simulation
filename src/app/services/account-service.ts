4fd23a76e4a6dd6453a105d63be794ba75e4e12c
jeff-romero <jeffromero86@gmail.com>
2026-06-22 14:39:49 -0700

    migrate account service progress from old repo

--- src/app/services/account-service.ts
+++ src/app/services/account-service.ts
@@ -1,6 +1,65 @@
 import { Injectable } from '@angular/core';
+import { Account } from '../shared/models/account';
+import { sampleAccounts } from '../../data';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { ACCOUNT_LOGIN_URL, ACCOUNTS_URL } from '../shared/constants/urls';
+import { IAccountLogin } from '../shared/interfaces/IAccountLogin';
+import { ToastrService } from 'ngx-toastr';
+
+const ACCOUNT_KEY = 'Account';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AccountService {}
+export class AccountService {
+  // this.getAccountFromLocalStorage()
+  private accountSubject = new BehaviorSubject<Account>(new Account());
+  public accountObservable: Observable<Account>;
+
+  constructor(private http:HttpClient, private toastrService: ToastrService) {
+    this.accountObservable = this.accountSubject.asObservable();
+  }
+
+  logIn(accountLogin: IAccountLogin): Observable<Account> {
+    return this.http.post<Account>(ACCOUNT_LOGIN_URL, accountLogin).pipe(
+      tap({
+        next: (account: any) => {
+          // this.setAccountToLocalStorage(account);
+          this.accountSubject.next(account);
+          this.toastrService.success(
+            `Welcome to Balanced Banking, ${account.firstName}`,
+            'Login successful'
+          );
+        },
+        error: (errorResponse: any) => {
+          this.toastrService.error(errorResponse.error, 'Login failed');
+        }
+      })
+    );
+  }
+
+  getAll(): Observable<Account[]> {
+    // TODO: if database connection fails, return sampleAccounts
+    // TODO: implement database connection
+    return this.http.get<Account[]>(ACCOUNTS_URL);
+  }
+
+  transferFunds(srcAccount: number, dstAccount: number, amount: number) {
+
+  }
+
+  // private setAccountToLocalStorage(account: Account) {
+  //   localStorage.setItem(ACCOUNT_KEY, JSON.stringify(account));
+  // }
+
+  // private getAccountFromLocalStorage(): Account {
+  //   const accountJson = localStorage.getItem(ACCOUNT_KEY);
+
+  //   if (accountJson) {
+  //     return JSON.parse(accountJson) as Account;
+  //   }
+
+  //   return new Account();
+  // }
+}
---
+++ src/data.ts
@@ -0,0 +1,102 @@
+import { Account } from './app/shared/models/account';
+
+export const sampleAccounts: any[] = [
+    {
+        accountNumber: 5555555555,
+        email: 'jdoe123@mymail.com',
+        password: '4ea5c508a6566e76240543f8feb06fd457777be39549c4016436afda65d2330e',
+        firstName: 'John',
+        lastName: 'Doe',
+        checkingBalance: 24563,
+        savingsBalance: 156798,
+        transactions: [
+            {
+                srcAccountNum: 5555555555,
+                dstAccountNum: 7429995109,
+                amount: 952,
+                dayOfWeek: 'Monday',
+                month: 'June',
+                dayOfMonth: 21,
+                year: 2026,
+                hour: 11,
+                minute: 17,
+                second: 2,
+                meridiem: 'AM',
+                timezone: 'GMT+01:00'
+            },
+            {
+                srcAccountNum: 5555555555,
+                dstAccountNum: 1212121212,
+                amount: 20,
+                dayOfWeek: 'Friday',
+                month: 'February',
+                dayOfMonth: 10,
+                year: 2026,
+                hour: 14,
+                minute: 53,
+                second: 47,
+                meridiem: 'PM',
+                timezone: 'GMT+01:00'
+            }
+        ]
+    },
+    {
+        accountNumber: 7429995109,
+        email: 'jsmith@realmail.com',
+        password: '8a5de6c53d649ae79d24cb64751443413156c659efa356bff2014d3ef40a1477',
+        firstName: 'Jane',
+        middleName: 'Capo',
+        lastName: 'Smith',
+        checkingBalance: 60721,
+        savingsBalance: 821112,
+        transactions: [
+            {
+                srcAccountNum: 5555555555,
+                dstAccountNum: 7429995109,
+                amount: 952,
+                dayOfWeek: 'Monday',
+                month: 'June',
+                dayOfMonth: 21,
+                year: 2026,
+                hour: 11,
+                minute: 17,
+                second: 2,
+                meridiem: 'AM',
+                timezone: 'GMT+01:00'
+            }
+        ]
+    },
+    {
+        accountNumber: 1212121212,
+        email: 'noxusforever@mymail.com',
+        password: 'a015d8406205ad1e6b8f76e55a7e7aeb85211a32e55f497ca1dbd78aac5c2666',
+        firstName: 'Darius',
+        lastName: 'Noxus',
+        checkingBalance: 503,
+        savingsBalance: 2407,
+        transactions: [
+            {
+                srcAccountNum: 5555555555,
+                dstAccountNum: 1212121212,
+                amount: 20,
+                dayOfWeek: 'Friday',
+                month: 'February',
+                dayOfMonth: 10,
+                year: 2026,
+                hour: 14,
+                minute: 53,
+                second: 47,
+                meridiem: 'PM',
+                timezone: 'GMT+01:00'
+            }
+        ]
+    },
+    {
+        accountNumber: 8945613373,
+        email: 'bronzehero2021@lanecamp.com',
+        password: '21bc36666163ff49018c0a51ba21d164582f3562ad147676b15f411b5f279e89',
+        firstName: 'Teemo',
+        lastName: 'Shrewm',
+        checkingBalance: 972
+    }
+];
