435cedb5e8228acdf748b467721b5c9c1b2639c2
jeff-romero <jeffromero86@gmail.com>
2026-06-22 14:42:55 -0700

    migrate app progress from old repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'login', component: Login }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Navigation } from './navigation/navigation';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterLink,
+    RouterOutlet,
+    Navigation,
+    Home,
+    Login,
+    ReactiveFormsModule,
+    // ToastrModule.forRoot({
+    //   timeOut:3000,
+    //   positionClass:'toast-bottom-right',
+    //   newestOnTop:false
+    // })
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('banking-simulation');
+  protected readonly title = signal('balanced-bank-simulation');
 }
